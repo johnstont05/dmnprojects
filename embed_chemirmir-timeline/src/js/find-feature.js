b57791a452c6a373baf35b4df02510321e9e8f0b
Taylor Johnston <tj369915@ohio.edu>
2019-10-31 00:11:49 -0400

    REMOVED

--- embed_chemirmir-timeline/src/js/find-feature.js
+++
@@ -1,7 +0,0 @@
-import $ from 'jquery';
-
-export default function (features) {
-  const currentHead = $('#headline').text();
-  const i = features.findIndex(feature => feature.properties.title === currentHead);
-  return i;
-}
--- interactive_texas-disd/gulp/tasks/browserify.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable strict */
-
-'use strict';
-
-/* eslint-enable strict */
-
-module.exports = require('./jsify')(false);
